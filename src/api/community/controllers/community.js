e9d78ff001ed27ab9ca53b2d79da8b3c0423b86d
medJemmoudi <med.jemmoudi@gmail.com>
2022-08-31 22:47:51 +0200

    feat(server): adding custom logic to Community controller

--- src/api/community/controllers/community.js
+++ src/api/community/controllers/community.js
@@ -1,9 +1,50 @@
-'use strict';
+"use strict";
 
 /**
  *  community controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::community.community');
+const getUserById = async (userId) => {
+  const user = await strapi.db.query("plugin::users-permissions.user").findOne({
+    where: {
+      id: {
+        $eq: userId,
+      },
+    },
+  });
+  return user;
+};
+
+module.exports = createCoreController(
+  "api::community.community",
+  ({ strapi }) => ({
+    async create(ctx) {
+      try {
+        const { name, winning_prize, access_code, userId } = ctx.request.body;
+        // Retrieve user
+        const user = await getUserById(userId);
+        // Create the community record
+        const record = await strapi.entityService.create(
+          "api::community.community",
+          {
+            data: {
+              name,
+              winning_prize,
+              access_code,
+              users: [user],
+            },
+          }
+        );
+
+        return { data: record };
+      } catch (e) {
+        return ctx.badRequest("Something went wrong", {
+          message: "Community cannot be created",
+          status: 500,
+        });
+      }
+    },
+  })
+);
