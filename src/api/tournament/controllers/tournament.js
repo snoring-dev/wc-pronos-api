577cdf8b56289a97427d8102765bce8c18411984
medJemmoudi <med.jemmoudi@gmail.com>
2022-10-25 16:19:12 +0200

    feat(api): re-organize all tournament data

--- src/api/tournament/controllers/tournament.js
+++ src/api/tournament/controllers/tournament.js
@@ -1,9 +1,40 @@
-'use strict';
+"use strict";
 
 /**
  *  tournament controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::tournament.tournament');
+module.exports = createCoreController(
+  "api::tournament.tournament",
+  ({ strapi }) => ({
+    async getAll() {
+      const cup = await strapi.db
+        .query("api::tournament.tournament")
+        .findMany({
+          populate: {
+            groups: {
+              populate: {
+                teams: {
+                  populate: {
+                    players: true,
+                  },
+                },
+                table: {
+                    populate: {
+                        entries: {
+                            populate: {
+                                team: true,
+                            }
+                        },
+                    },
+                },
+              },
+            },
+          },
+        });
+      return cup;
+    },
+  })
+);
--- src/api/tournament/routes/tournament.js
+++ src/api/tournament/routes/tournament.js
@@ -1,9 +1,54 @@
-'use strict';
+"use strict";
 
 /**
- * tournament router.
+ * community router.
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
+const originalRouter = createCoreRouter('api::tournament.tournament');
 
-module.exports = createCoreRouter('api::tournament.tournament');
+const defaultRouter = (innerRouter, routeOveride = [], extraRoutes = []) => {
+  let routes;
+
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes;
+
+      const newRoutes = routes.map((route) => {
+        let found = false;
+
+        routeOveride.forEach((overide) => {
+          if (
+            route.handler === overide.handler &&
+            route.method === overide.method
+          ) {
+            found = overide;
+          }
+        });
+
+        return found || route;
+      });
+
+      return extraRoutes.concat(newRoutes);
+    },
+  };
+};
+
+const myExtraRoutes = [
+  {
+    method: "GET",
+    path: "/tournaments/all",
+    handler: "api::tournament.tournament.getAll",
+    config: {
+      auth: false,
+    },
+  },
+];
+
+const myOverideRoute = [];
+
+module.exports = defaultRouter(originalRouter, myOverideRoute, myExtraRoutes);
+
