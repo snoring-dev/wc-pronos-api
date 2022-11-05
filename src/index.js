242a9c4e23a38dd50a13b43127c40cf1911f2cb8
medJemmoudi <med.jemmoudi@gmail.com>
2022-11-05 18:33:07 +0100

    feat(sockets): install socket.io and add it to the core server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-'use strict';
-
+"use strict";
 module.exports = {
   /**
    * An asynchronous register function that runs before
@@ -7,7 +6,7 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {},
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -16,5 +15,19 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+    var io = require("socket.io")(strapi.server.httpServer, {
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+      },
+    });
+    io.on("connection", function (socket) {
+      console.log(`====> SOCKET: ${socket.id} user just connected! <====`);
+      socket.on('disconnect', () => {
+        socket.disconnect()
+        console.log('====> SOCKET: A user disconnected <====');
+      });
+    });
+  },
 };
